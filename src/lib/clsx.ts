7492a18794dcde51400fd58a673b0fb35d0797bd
ahnafms <ahnafmusyaffa47@gmail.com>
2023-10-06 19:31:21 +0700

    feat: wrap clsx with twmerge

--- src/lib/clsx.ts
+++
@@ -1,9 +0,0 @@
-import _clsx, { ClassValue } from 'clsx';
-
-const clsx = (...args: ClassValue[]) =>
-  _clsx(...args)
-    .replace(/^\n/, '')
-    .replace(/\n/g, ' ')
-    .replace(/\s+/g, ' ');
-
-export default clsx;
