2d0a849235541592c04dc3d1e113becf7a24d812
reynoldputra <reynoldputra1@gmail.com>
2023-11-23 21:18:42 +0700

    feat: vercel analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Anton, Epilogue } from 'next/font/google';
 
@@ -31,6 +32,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={clsxm(epilogue.variable, anton.variable)}>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
