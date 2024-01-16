f7975b2ff0a166cb7abce24c76dd896a0e47bf3d
reynoldputra <reynoldputra1@gmail.com>
2024-01-16 11:01:25 +0700

    fix: take out paper background on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -341,13 +341,14 @@ export default function About() {
           <section className='h-full flex flex-col justify-center items-center overflow-x-hidden relative pb-20 z-[5]'>
             <Division />
           </section>
-          <Image
-            src='/images/about/paper5.png'
-            alt='paper'
-            width={500}
-            height={5000}
-            className='absolute h-full top-0 w-full z-[0] bg-cover'
-          />
+          {/* TODO: Performance issue */}
+          {/* <Image */}
+          {/*   src='/images/about/paper5.png' */}
+          {/*   alt='paper' */}
+          {/*   width={500} */}
+          {/*   height={5000} */}
+          {/*   className='absolute h-full top-0 w-full z-[0] bg-cover' */}
+          {/* /> */}
         </section>
         <Footer />
       </main>
