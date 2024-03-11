85dfa38da50e957e76aa6c561b3ce3bacdc4d626
reynoldputra <reynoldputra1@gmail.com>
2024-03-11 14:56:45 +0700

    fix: ignore midleware on preview

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  if (process.env.NODE_ENV == 'development') {
+  if (
+    process.env.NODE_ENV == 'development' ||
+    ['development', 'preview'].includes(process.env.VERCEL_ENV ?? '')
+  ) {
     return NextResponse.next();
   }
   return NextResponse.redirect(new URL('/coming-soon', request.url));
